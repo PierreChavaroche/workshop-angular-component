dfe6bfd62804a06f90ec9ad878873bfeb9bea488
Pierre Chavaroche <info@chavaroche.de>
2020-08-01 13:29:49 +0200

    Renaming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-example-component'`, () => {
+  it(`should have as title 'workshop-angular-component'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-example-component');
+    expect(app.title).toEqual('workshop-angular-component');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-example-component app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('workshop-angular-component app is running!');
   });
 });
