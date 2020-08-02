1470d84785fe7dd05b4f627e01d22b4631a54702
Pierre Chavaroche <info@chavaroche.de>
2020-08-02 11:01:58 +0200

    ViewChild example added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'workshop-angular-component';
+  @ViewChild(HomeComponent) private homeComponent: HomeComponent;
+
+  ngOnInit() {
+    console.log(this.homeComponent);
+  }
+
+  ngAfterViewInit() {
+    // child views loaded
+    console.log(this.homeComponent);
+  }
 }
